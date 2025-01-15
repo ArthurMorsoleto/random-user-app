25d550addb1a153fe4d32599ea1fbe23ff51e0ac
Arthur M Batista <arthur.morsoleto@gmail.com>
2025-01-14 21:31:11 -0300

    add new todos

--- app/src/main/java/com/amb/randomusersapp/presentation/RandomUserViewModel.kt
+++ app/src/main/java/com/amb/randomusersapp/presentation/RandomUserViewModel.kt
@@ -2,6 +2,7 @@ package com.amb.randomusersapp.presentation
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.amb.randomusersapp.common.Response
 import com.amb.randomusersapp.domain.usecase.RandomUserUseCase
 import kotlinx.coroutines.launch
 
@@ -11,7 +12,19 @@ class RandomUserViewModel(
 
     fun getRandomUser() = viewModelScope.launch {
         useCase().collect { result ->
-            println(result)
+            when (result) {
+                is Response.Success -> {
+                    // TODO show user info
+                }
+
+                is Response.Error -> {
+                    // TODO show error
+                }
+
+                is Response.Loading -> {
+                    // TODO show loading
+                }
+            }
         }
     }
 }
