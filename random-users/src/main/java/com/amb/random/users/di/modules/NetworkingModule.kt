d626bd30623fa63820cc9f6370a8e022e1ddb24b
Arthur M Batista <arthur.morsoleto@gmail.com>
2025-01-16 21:14:54 -0300

    update api buider param

--- random-users/src/main/java/com/amb/random/users/di/modules/NetworkingModule.kt
+++ random-users/src/main/java/com/amb/random/users/di/modules/NetworkingModule.kt
@@ -1,10 +1,10 @@
 package com.amb.random.users.di.modules
 
-import com.amb.network.ApiBuilderImpl
+import com.amb.network.ApiBuilder
 import com.amb.random.users.data.remote.RandomApi
 import org.koin.dsl.module
 
-internal class NetworkingModule(apiBuilder: ApiBuilderImpl) {
+internal class NetworkingModule(apiBuilder: ApiBuilder) {
 
     val networkModule = module {
         factory { apiBuilder.build(RandomApi::class) }
